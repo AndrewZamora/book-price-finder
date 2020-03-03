dc753f1a8a1ca8b38ab4b75b37c0bb6cbf7143bd
Andrew Zamora <andrewmichaelzamora@gmail.com>
2020-03-03 23:01:49 +0900

    Initial Commit

---
+++ scrapper.js
@@ -0,0 +1,88 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+async function scrapeBook(url) {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(url);
+    const [element] = await page.$x('/html/body/div[1]/div[1]/div/table/tbody/tr[1]/td[1]/a/img');
+    const src = await element.getProperty('src');
+    const imageURL = await src.jsonValue();
+    browser.close();
+    return { imageURL }
+}
+
+async function screenshotPage(url) {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.goto(url);
+    await page.screenshot({ path: 'test.png', fullPage: true });
+    browser.close();
+}
+async function getBookByISBN(url, isbn) {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.goto(url);
+    await page.focus('#search_form > table > tbody > tr > td > table > tbody > tr:nth-child(5) > td > input[type=text]');
+    await page.keyboard.type(isbn)
+    await page.click('#submitBtn')
+    await page.waitForSelector('#header-section-context > span')
+    await page.screenshot({ path: 'test.png', fullPage: true });
+    browser.close();
+}
+
+async function getBookByTitleAndAuthor(title, author) {
+    const url = 'https://www.bookfinder.com/';
+    const browser = await puppeteer.launch({ headless: false });
+    try {
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1280, height: 800 });
+        await page.goto(url);
+        await page.focus('#author');
+        await page.keyboard.type(author);
+        await page.focus('#title');
+        await page.keyboard.type(title);
+        await page.click('#submitBtn');
+        await page.waitForFunction(() => {
+            if (document.querySelector('.select-authorname')) {
+                results = "success";
+                return true;
+            }
+        });
+        await page.screenshot({ path: `./screenshots/${title}-${author}.png`, fullPage: true });
+        await browser.close();
+    } catch(err) {
+        console.log(err)
+        await browser.close();
+    }
+}
+
+const readFile = (...args) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(...args, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(data);
+        });
+    });
+}
+
+(async () => {
+    const rawBookData = await readFile('./test.tsv', 'utf8').catch(err => console.log(err));
+    const books = rawBookData.split('\n').splice(1, rawBookData.length - 1).map(item => {
+        const row = item.split('\t');
+        return {
+            description: row[2],
+            title: row[3],
+            author: row[4]
+        };
+    });
+    console.log(books);
+    // for (const book of books) {
+    //     await getBookByTitleAndAuthor(book.title, book.author).catch(err => browser.close());
+    // }
+    // await getBookByTitleAndAuthor(books[2].title, books[2].author).catch(err => console.log(err))
+})()
